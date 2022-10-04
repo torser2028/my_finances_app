815935225ad1e7df7e04d56cc20b0a323b5b4be1
Alejandro Toro Serna <altose87@gmail.com>
2022-10-04 16:31:51 -0500

    Fix the error in the createApi

--- src/redux/api/categories.js
+++ src/redux/api/categories.js
@@ -3,14 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const categoriesApi = createApi({
   reducerPath: "categoriesApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/api/v1" }),
-  endpoints: (builder) => {
+  endpoints: (builder) => ({
     getCategories: builder.query({
       query: () => ({
         url: "/categories",
         method: "GET",
       }),
-    });
-  },
+    }),
+  }),
 });
 
 export const { useGetCategoriesQuery } = categoriesApi;
