11f6aa3971f7d6b8f879a848dd290d39eb09c46f
Alejandro Toro Serna <altose87@gmail.com>
2023-04-19 18:07:27 -0500

    Fix the categories page

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { categoriesApi } from "../redux/api/categories";
 
 export const store = configureStore({
@@ -9,3 +9,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(categoriesApi.middleware),
 });
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch);
