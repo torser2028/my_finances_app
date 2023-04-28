6c48e3a390f6eaf6e461687c05bda0c60b14171a
Alejandro Toro Serna <altose87@gmail.com>
2023-04-28 13:22:29 -0500

    remove jest

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
--- src/redux/api/categories.js
+++ src/redux/api/categories.js
@@ -1,8 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import fetch from 'isomorphic-fetch';
 
 export const categoriesApi = createApi({
   reducerPath: 'categoriesApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/api/v1' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3001/api/v1',
+    fetchFn: fetch,
+  }),
   endpoints: (builder) => ({
     getCategories: builder.query({
       query: () => ({
