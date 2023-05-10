8e8ca0166c900c0b19efd2a734f699d54f508655
Alejandro Toro <altose87@gmail.com>
2023-05-10 13:17:45 -0700

    Update the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import Checker from 'vite-plugin-checker';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Checker({ typescript: true }),
+  ],
+});
